e473c9b4d7848773ff41ebbe28502bcff72d3adf
John Snyders <jsnyders04@gmail.com>
2015-04-12 23:56:55 -0400

    add autoIndentWriter, add very simple hand coded template test WIP.

---
+++ test/hello.st.js
@@ -0,0 +1,22 @@
+/*
+ * hand crafted template
+ */
+
+function getInstance(st, group) {
+    "use strict";
+
+    /*
+     Template hello.st:
+     hello(audience) ::= <%Hello $audience;null="is anyone there?"$!$!this is the syntax for a comment inside a template!$
+     %>
+     */
+    group.addTemplate("hello", function (c, w) {
+        w.write("\nHello ");
+        st.write(w, c.audience, {"null": "is anyone there?"});
+        w.write("!\n");
+    }); // xxx need info about args, perhaps add as properties of the function
+
+    return group;
+}
+
+module.exports = getInstance;
---
+++ test/helloTest.js
@@ -0,0 +1,24 @@
+/*global describe, it*/
+
+"use strict";
+
+var assert = require("assert"),
+    st = require("../lib/stRuntime"),
+    w = require("../lib/autoIndentWriter"),
+    helloTemplateGroup = require("./hello.st");
+
+describe("hello world test", function() {
+
+    it("should generate the expected string", function() {
+        var t,
+            group = st.loadGroup(helloTemplateGroup),
+            writer = w.makeWriter();
+
+        t = group.getTemplate("hello");
+        assert.notStrictEqual(t, null, "found a template");
+        t.add("audience", "world");
+        t.write(writer);
+        console.log("xxx result: " + writer.toString());
+        assert.strictEqual(writer.toString(), "Hello world!\n", "got expected rendered text");
+    });
+});
