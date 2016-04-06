8fc3d3dbb2a176fc4d4e0e9a8f22d00347664c45
pjmolina <pjmolina@gmail.com>
2016-04-06 22:30:20 +0200

    fix string tests on Windows/Unix to take in account EOL chars.

--- test/helloTest.js
+++ test/helloTest.js
@@ -18,7 +18,7 @@ describe("hello world test", function() {
         assert.notStrictEqual(t, null, "found a template");
         t.add("audience", "world");
         t.write(writer);
-        assert.strictEqual(writer.toString(), "Hello world!\n", "got expected rendered text");
+        assert.strictEqual(writer.toString(), "Hello world!" + writer.eol, "got expected rendered text");
     });
 
     it("should generate the expected string with null input", function() {
@@ -30,7 +30,7 @@ describe("hello world test", function() {
         assert.notStrictEqual(t, null, "found a template");
         t.add("audience", null);
         t.write(writer);
-        assert.strictEqual(writer.toString(), "Hello is anyone there?!\n", "got expected rendered text");
+        assert.strictEqual(writer.toString(), "Hello is anyone there?!" + writer.eol, "got expected rendered text");
     });
 
     it("should generate the expected string with an array", function() {
@@ -42,7 +42,7 @@ describe("hello world test", function() {
         assert.notStrictEqual(t, null, "found a template");
         t.add("audience", ["Bob", "Sue", "Dan"]);
         t.write(writer);
-        assert.strictEqual(writer.toString(), "Hello Bob, Sue, Dan!\n", "got expected rendered text");
+        assert.strictEqual(writer.toString(), "Hello Bob, Sue, Dan!" + writer.eol, "got expected rendered text");
     });
 
     it("should generate the expected string with a single item array", function() {
@@ -54,7 +54,7 @@ describe("hello world test", function() {
         assert.notStrictEqual(t, null, "found a template");
         t.add("audience", ["world"]);
         t.write(writer);
-        assert.strictEqual(writer.toString(), "Hello world!\n", "got expected rendered text");
+        assert.strictEqual(writer.toString(), "Hello world!" + writer.eol, "got expected rendered text");
     });
 
     it("should generate the expected string with an empty array", function() {
@@ -66,7 +66,7 @@ describe("hello world test", function() {
         assert.notStrictEqual(t, null, "found a template");
         t.add("audience", []);
         t.write(writer);
-        assert.strictEqual(writer.toString(), "Hello !\n", "got expected rendered text");
+        assert.strictEqual(writer.toString(), "Hello !" + writer.eol, "got expected rendered text");
     });
 
     it("should generate the expected string when there is renderer", function() {
@@ -84,6 +84,6 @@ describe("hello world test", function() {
         assert.notStrictEqual(t, null, "found a template");
         t.add("audience", "world");
         t.write(writer, rc);
-        assert.strictEqual(writer.toString(), "Hello WORLD!\n", "got expected rendered text");
+        assert.strictEqual(writer.toString(), "Hello WORLD!" + writer.eol, "got expected rendered text");
     });
 });
